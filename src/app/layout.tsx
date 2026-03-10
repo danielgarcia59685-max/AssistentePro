35cd30df0b5831fe999ff31877340593177504e2
danielgarcia59685-max <danielgarcia59685@gmail.com>
2026-03-10 13:16:02 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next'
+import type { ReactNode } from 'react'
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import '../lib/fonts'
 import { Toaster } from '../components/ui/toaster'
-import { Providers } from '@/context/Providers'
+import Providers from '@/context/Providers'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,7 +29,7 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: ReactNode
 }>) {
   return (
     <html lang="pt-BR" suppressHydrationWarning>
